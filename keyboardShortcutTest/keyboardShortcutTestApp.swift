f4840c086813485ec91da17ba218742b2d8067f8
Keisuke　Chinone (Iroiro) <iroiro.work1234@gmail.com>
2022-11-13 22:44:09 +0900

    Initial Commit

---
+++ keyboardShortcutTest/keyboardShortcutTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyboardShortcutTestApp.swift
+//  keyboardShortcutTest
+//
+//  Created by 茅根啓介 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct keyboardShortcutTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
